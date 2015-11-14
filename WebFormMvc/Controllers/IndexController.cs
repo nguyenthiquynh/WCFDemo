ba6da3c14a15f691466be940c693166cb72dcf28
nguyenthiquynh <nguyenthiquynh.1112@gmail.com>
2015-11-14 21:44:45 +0700

    nothing

---
+++ WebFormMvc/Controllers/IndexController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebFormMvc.ServiceReferenceSinhVien;
+using WebFormMvc.ServiceReferenceDemo;
+namespace WebFormMvc.Controllers
+{
+    public class IndexController : Controller
+    {
+        //
+        // GET: /Index/
+
+        public ActionResult Index()
+        {
+            List<SinhVien> ListSV = new List<SinhVien>();
+            ServiceSinhVienClient sv = new ServiceSinhVienClient();
+            ViewBag.ListSV = sv.ListSinhVien().ToList();
+            return View();
+        }
+
+    }
+}
---
+++ WebFormMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebFormMvc.MvcApplication" Language="C#" %>
